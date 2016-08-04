d5b91d56cc9f9956f2ca6da8afca3391cfa6b94e
mailey9 <minorlife99@gmail.com>
2016-08-04 23:22:04 +0900

    01-Tetris-003-2016-08-04-RotateTest

--- ConsoleTetris/ConsoleTetris/_PlayGround.cpp
+++ ConsoleTetris/ConsoleTetris/_PlayGround.cpp
@@ -1,10 +1,45 @@
-// cpp002-2016-08-04-09:30 IN
+// cpp003-2016-08-04-19:30 IN
+//////////////////////////////////////////////////////////////////
+// *. block generator, FILE I/O를 이용하여 ...
+// 기존 하노이탑 과제에서 수행한 과정은..
+// fwrite를 이용해 전과정을 파일에 기록한 뒤,
+// 파일 커서를 [임의 위치]로 이동 한 뒤에 그를
+// fread로 읽어오는 방식이었다.
+//
+// 테트리스의 경우에는 읽어들이는 길이가 -유동적- 일 수 있다.
+// 순서-타입을 묶음으로 기록할 수도 있으나 덩치도 크고 의미도 없으니,
+// 헤드에 총 길이를 넣고 [100] 이면, 100개 임을 명시하고,
+// 동적으로 블록시퀀스를 기록할 변수에 메모리를 할당하면 될 것이다.
+//
+// *. C++에 대한 기초적인 문법적 이해도와 함께
+// *. 기본적인 Game 싸이클에 대한 이해도가 부족해 보이고.. (백버퍼를 응용한)
+// *. 마찬가지로 Draw에 대한 것도 추상적으로 밖에 모르는 것 같이 느껴진다.
+//////////////////////////////////////////////////////////////////
 #include "stdafx.h"
 
 #define MAX_BLOCK_SIZE 4
-#define TEST_LOG_ON 1
 
-enum BUnitColor {
+#define BOARD_SIZE_ROW      20
+#define BOARD_SIZE_COL      20
+
+#define BOARD_BACK 0
+#define BOARD_FACE 1
+
+HANDLE                  g_hConsole = INVALID_HANDLE_VALUE;
+void SetCursorPosition(int x, int y) {
+	COORD coord = { (SHORT)x, (SHORT)y };
+	SetConsoleCursorPosition(g_hConsole, coord);
+}
+
+enum EKeys {
+	KEY_NULL = 0,
+	KEY_LEFT = 'a',
+	KEY_RIGHT = 'd',
+	KEY_ROTATE = 'w',	// added
+	KEY_ESC = 27,
+};//enum EKeys
+
+enum EBUnitColor {
 	//EMPTY = -1,
 	EMPTY = 0,
 	DEFAULT, // 1
@@ -15,7 +50,7 @@ enum BUnitColor {
 struct BlockUnit{
 	int x;
 	int y;
-	BUnitColor color;
+	EBUnitColor color;
 };
 
 class BlockQuad{
@@ -30,41 +65,54 @@ protected:
 		m_iNumBlock(MAX_BLOCK_SIZE),
 		m_iBlockRotation(0),
 		m_ciBlockLimiter(in_limiter){
-		if (TEST_LOG_ON) printf("PARENT - EXP. OVL. without (bMsg) CONSTRUCTOR called\r\n");
 	}
-	BlockQuad(bool bMsg, int in_limiter) :
+public:
+	BlockQuad() :
 		m_iNumBlock(MAX_BLOCK_SIZE),
 		m_iBlockRotation(0),
-		m_ciBlockLimiter(in_limiter){
-		if (TEST_LOG_ON) printf("PARENT - EXP. OVL. with (bMsg) CONSTRUCTOR called\r\n");
-		if (bMsg) printf("PARENT - Set m_iNumBlock to '%i(MAX_BLOCKSIZE)'\r\n", MAX_BLOCK_SIZE);
+		m_ciBlockLimiter(0){
+		for (int i = 0; i < m_iNumBlock; ++i){
+			m_aBlockPosition[i].x = 0;
+			m_aBlockPosition[i].y = 0;
+			m_aBlockPosition[i].color = EMPTY;
+		}
 	}
+	virtual ~BlockQuad(){}
 
-	void SetQuad_Color(BUnitColor in_color){
+	virtual void SetRotation(){}
+	virtual void Show(){}
+
+	// common setter & getters
+	void SetUnit(int in_index, int in_x, int in_y){
+		m_aBlockPosition[in_index].x = in_x;
+		m_aBlockPosition[in_index].y = in_y;
+	}
+	void SetQuad_Color(EBUnitColor in_color){
 		for (int i = 0; i < MAX_BLOCK_SIZE; i++)
 			m_aBlockPosition[i].color = in_color;
 	}
-public:
-	~BlockQuad(){
-		if (TEST_LOG_ON) printf("PARENT - DEF. DESTROYER called\r\n");
+	const BlockQuad* GetQuad(){
+		return this;
 	}
-
-	virtual void SetRotation(bool bMsg){}
-
-	virtual void Show(){}
-	virtual void Display(){}
+	const BlockUnit* GetUnit(int in_index){
+		return &(this->m_aBlockPosition[in_index]);
+	}
 };
 
 class BlockType_A : public BlockQuad{
 public:
-	BlockType_A() : BlockQuad(2){// 상속받은 자식클래스가 부모의 명시적 생성자를 호출하는 방법.
-		if (TEST_LOG_ON) printf("CHILD - DEF. without (bMsg) CONSTRUCTOR called\r\n");}
-	BlockType_A(bool bMsg) : BlockQuad(bMsg, 2){
-		if (TEST_LOG_ON) printf("CHILD - EXP. OVL. with (bMsg) CONSTRUCTOR called\r\n");}
-	~BlockType_A(){
-		if (TEST_LOG_ON) printf("CHILD - DEF. DESTROYER called\r\n");}
+	BlockType_A() : BlockQuad(2){
+		Init_temp();
+	}
+	virtual ~BlockType_A(){}								// -- NOTE 1 -- 가상파괴자
 
-	virtual void SetRotation(bool bMsg){
+	void Init_temp(){
+		m_aBlockPosition[0].x = 0; m_aBlockPosition[0].y = 0; m_aBlockPosition[0].color = DEFAULT;
+		m_aBlockPosition[1].x = 0; m_aBlockPosition[1].y = 1; m_aBlockPosition[1].color = DEFAULT;
+		m_aBlockPosition[2].x = 0; m_aBlockPosition[2].y = 2; m_aBlockPosition[2].color = DEFAULT;
+		m_aBlockPosition[3].x = 0; m_aBlockPosition[3].y = 3; m_aBlockPosition[3].color = DEFAULT;
+	}
+	virtual void SetRotation(){
 		m_iBlockRotation = (m_iBlockRotation + 1) % m_ciBlockLimiter;
 
 		if (m_iBlockRotation == 0){
@@ -81,318 +129,206 @@ public:
 		}
 		else
 			exit(1); // error, m_iBlockRotation should be from 0 to 1.
-
-		if (bMsg){
-			printf("\tcurrent iBlockRotation = %i\r\n", m_iBlockRotation);
-			printf("\tcurrent this block members' local coordinates are ..\r\n");
-			for (int i = 0; i < MAX_BLOCK_SIZE; i++)
-				printf("\tINDEX: %i\tX : %i\tY : %i\r\n", i, m_aBlockPosition[i].x, m_aBlockPosition[i].y);
-		}
 	}//SetRotation
-
-	void Show(){
+	virtual void Show(){
 		printf("\t< ShowThis >\r\n");
 		printf("\tCONSTANT ciBlockLimiter = %i\r\n", m_ciBlockLimiter);
 		printf("\tcurrent iBlockRotation  = %i\r\n", m_iBlockRotation);
 		printf("\tcurrent this block members' local coordinates are ..\r\n");
 		for (int i = 0; i < MAX_BLOCK_SIZE; i++)
 			printf("\tINDEX: %i\tX : %i\tY : %i\r\n", i, m_aBlockPosition[i].x, m_aBlockPosition[i].y);
-	}
-	void Display(){
+	}//SHow .. for test.
+};//BlockType_A
 
-	}
-};
-
-void main()
-{
-	BlockQuad* pTest;
-
-	char TestBlock = 0;
-	char input = 0;
-
-	std::cout << "Select Block-Type" << std::endl
-			<< "  Type A : a, A" << std::endl
-			<< "  Type B : b, B" << std::endl
-			<< "  Type C : c, C" << std::endl
-			<< "  Type D : d, D" << std::endl
-			<< "  Type E : e, E" << std::endl
-			<< "  Type F : f, F" << std::endl
-			<< "  Type G : g, G" << std::endl
-			<< "  _quit_ : ESC" << std::endl;
-	//<< "  command> " << std::endl;
-
-	while (1){
-		std::cout << "   command> ";
-		std::cin >> TestBlock;
-		if (TestBlock == 'a' || TestBlock == 'A'){
-			//pTest = new BlockType_A(true);//!!new(1)
-			pTest = new BlockType_A();//!!new(1)
-			break;
-		}
-		//else if (TestBlock == 'b' || TestBlock == 'B'){
-		//	pTest = new BlockType_B(true);
-		//	break;
-		//}
-		else
-		{
-			std::cout << "   ! warning (wrong Block-Type)" << std::endl;
-			continue;
-		}
-	}
-
-	std::cout << "Testing (SetRotation)" << std::endl
-			<< "   a key : Proceed" << std::endl
-			<< "   s key : Stop and Quit" << std::endl;
-	while (1)
-	{
-		if (_kbhit())
-		{
-			input = _getch();
-
-			if (input == 'a')
-				pTest->SetRotation(true);
-			else if (input == 's')
-				break;
-			else
-				continue;
-		}
-	}
-	//BlockType_A TestOff(false);
-	//return;
-
-	delete(pTest);//!!delete(1)
-}
-
-/*
-
-//BlockQuad - parental o.
-//NOTE : 생성자가 보호범위 안에 있으면 외부함수 (상속 이외의 함수) 에서는 생성이 불가능해보인다. 그리고 의도상 이것이 맞다.
-class BlockQuad{
-private:	// protected? // 상속자라도 접근할 필요가 없는 변수일 것이다.
-	int m_iNumBlock;
-protected:
-	BlockUnit m_aBlockPosition[MAX_BLOCK_SIZE];
-	int m_iBlockRotation;
-	const int m_ciBlockLimiter;
-
-	BlockQuad(int in_limiter) :
-		m_iNumBlock(MAX_BLOCK_SIZE),
-		m_iBlockRotation(0),
-		m_ciBlockLimiter(in_limiter){
-		if (TEST_LOG_ON) printf("PARENT - EXP. OVL. without (bMsg) CONSTRUCTOR called\r\n");
-	}
-	BlockQuad(bool bMsg, int in_limiter) :
-		m_iNumBlock(MAX_BLOCK_SIZE),
-		m_iBlockRotation(0),
-		m_ciBlockLimiter(in_limiter){
-		if (TEST_LOG_ON) printf("PARENT - EXP. OVL. with (bMsg) CONSTRUCTOR called\r\n");
-		if (bMsg){
-			printf("PARENT - Set m_iNumBlock to '%i(MAX_BLOCKSIZE)'\r\n", MAX_BLOCK_SIZE);
-		}//if
-	}
-
-	void SetUnit(int in_index, int in_x, int in_y);
-	void SetUnit(int in_index, int in_x, int in_y, BUnitColor in_color);
-	void SetQuad_Color(BUnitColor in_color);
+class BlockType_B : public BlockQuad{
 public:
-	~BlockQuad(){
-		if (TEST_LOG_ON) printf("PARENT - DEF. DESTROYER called\r\n");
+	BlockType_B() : BlockQuad(4){
+		Init_temp();
 	}
+	virtual ~BlockType_B(){}
 
-	virtual void SetRotation(){}	// 기존 회전함수를 응용하는 것보단 이것이 맞다... 이름도 점검해볼 필요는 있다..
-	virtual void SetRotation(bool bMsg){}
-
-	virtual void Show(){}
-	virtual void Display(){}
-};
-
-void BlockQuad::SetUnit(int in_index, int in_x, int in_y){
-	m_aBlockPosition[in_index].x = in_x;
-	m_aBlockPosition[in_index].y = in_y;
-}//SetUnit
-void BlockQuad::SetUnit(int in_index, int in_x, int in_y, BUnitColor in_color){
-	m_aBlockPosition[in_index].x = in_x;
-	m_aBlockPosition[in_index].y = in_y;
-	m_aBlockPosition[in_index].color = in_color;
-	//m_aBlockPosition[in_index].color = (int)in_color;	//typecasting을 이용한 . . .
-}//overloaded SetUnit with Unit's Color.
-void BlockQuad::SetQuad_Color(BUnitColor in_color){
-	for (int i = 0; i < MAX_BLOCK_SIZE; i++)
-		m_aBlockPosition[i].color = in_color;
-}
-
-//BlockType_A // .. A - - - -
-class BlockType_A : public BlockQuad{
-public:
-	BlockType_A() : BlockQuad(2){
-		//SetRotation();
-		if (TEST_LOG_ON) printf("CHILD - DEF. without (bMsg) CONSTRUCTOR called\r\n");
+	void Init_temp(){
+		m_aBlockPosition[0].x = 0; m_aBlockPosition[0].y = 0; m_aBlockPosition[0].color = DEFAULT;	//
+		m_aBlockPosition[1].x = 1; m_aBlockPosition[1].y = 0; m_aBlockPosition[1].color = DEFAULT;	// ★3
+		m_aBlockPosition[2].x = 0; m_aBlockPosition[2].y = 1; m_aBlockPosition[2].color = DEFAULT;	// ★2
+		m_aBlockPosition[3].x = 0; m_aBlockPosition[3].y = 2; m_aBlockPosition[3].color = DEFAULT;	// ☆0★1
 	}
-	BlockType_A(bool bMsg) : BlockQuad(bMsg, 2){	// 상속받은 자식클래스가 부모의 명시적 생성자를 호출하는 방법.
-		if (TEST_LOG_ON) printf("CHILD - EXP. OVL. with (bMsg) CONSTRUCTOR called\r\n");
-	}
-	~BlockType_A(){
-		if (TEST_LOG_ON) printf("CHILD - DEF. DESTROYER called\r\n");
-	}
-
 	virtual void SetRotation(){
 		m_iBlockRotation = (m_iBlockRotation + 1) % m_ciBlockLimiter;
 
 		if (m_iBlockRotation == 0){
-			SetUnit(0, 0, 0);	// ★3
-			SetUnit(1, 0, 1);	// ★2
-			SetUnit(2, 0, 2);	// ★1
-			SetUnit(3, 0, 3);	// ☆0
+			m_aBlockPosition[0].x = 0; m_aBlockPosition[0].y = 0;	//
+			m_aBlockPosition[1].x = 1; m_aBlockPosition[1].y = 0;	// ★3
+			m_aBlockPosition[2].x = 0; m_aBlockPosition[2].y = 1;	// ★2
+			m_aBlockPosition[3].x = 0; m_aBlockPosition[3].y = 2;	// ☆0★1
 		}
 		else if (m_iBlockRotation == 1){
-			SetUnit(0, 0, 0);	//
-			SetUnit(1, 1, 0);	//
-			SetUnit(2, 2, 0);	// 0 1 2 3
-			SetUnit(3, 3, 0);	// ☆★★★
+			m_aBlockPosition[0].x = 0; m_aBlockPosition[0].y = 0;	//
+			m_aBlockPosition[1].x = 1; m_aBlockPosition[1].y = 0;	//
+			m_aBlockPosition[2].x = 2; m_aBlockPosition[2].y = 0;	// 　 　 ★3
+			m_aBlockPosition[3].x = 2; m_aBlockPosition[3].y = 1;	// ☆0★1★2
 		}
-		else
-			exit(1); // error, m_iBlockRotation should be from 0 to 1.
-	}//SetRotation
-	virtual void SetRotation(bool bMsg){
-		m_iBlockRotation = (m_iBlockRotation + 1) % m_ciBlockLimiter;
-
-		if (m_iBlockRotation == 0){
-			SetUnit(0, 0, 0);	// ★3
-			SetUnit(1, 0, 1);	// ★2
-			SetUnit(2, 0, 2);	// ★1
-			SetUnit(3, 0, 3);	// ☆0
+		else if (m_iBlockRotation == 2){
+			m_aBlockPosition[0].x = 1; m_aBlockPosition[0].y = 0;	//
+			m_aBlockPosition[1].x = 1; m_aBlockPosition[1].y = 1;	// ★2★3
+			m_aBlockPosition[2].x = 0; m_aBlockPosition[2].y = 2;	// 　 ★1
+			m_aBlockPosition[3].x = 1; m_aBlockPosition[3].y = 2;	// 　 ☆0
 		}
-		else if (m_iBlockRotation == 1){
-			SetUnit(0, 0, 0);	//
-			SetUnit(1, 1, 0);	//
-			SetUnit(2, 2, 0);	// 0 1 2 3
-			SetUnit(3, 3, 0);	// ☆★★★
+		else if (m_iBlockRotation == 3){
+			m_aBlockPosition[0].x = 0; m_aBlockPosition[0].y = 0;	//
+			m_aBlockPosition[1].x = 0; m_aBlockPosition[1].y = 1;	// 
+			m_aBlockPosition[2].x = 1; m_aBlockPosition[2].y = 1;	// ★1★2★3
+			m_aBlockPosition[3].x = 2; m_aBlockPosition[3].y = 1;	// ☆0
 		}
-		else
-			exit(1); // error, m_iBlockRotation should be form 0 to 1.
-
-		if (bMsg){
-			printf("\tcurrent iBlockRotation = %i\r\n", m_iBlockRotation);
-			printf("\tcurrent this block members' local coordinates are ..\r\n");
-			for (int i = 0; i < MAX_BLOCK_SIZE; i++)
-				printf("\tINDEX: %i\tX : %i\tY : %i\r\n", i, m_aBlockPosition[i].x, m_aBlockPosition[i].y);
+		else{
+			printf("%i", m_iBlockRotation);
+			printf("잡았다 이놈");
+			exit(1);
 		}
-	}//overloaded-SetRotation with message.
-
-	void Show(){
+	}
+	virtual void Show(){
 		printf("\t< ShowThis >\r\n");
 		printf("\tCONSTANT ciBlockLimiter = %i\r\n", m_ciBlockLimiter);
 		printf("\tcurrent iBlockRotation  = %i\r\n", m_iBlockRotation);
 		printf("\tcurrent this block members' local coordinates are ..\r\n");
 		for (int i = 0; i < MAX_BLOCK_SIZE; i++)
 			printf("\tINDEX: %i\tX : %i\tY : %i\r\n", i, m_aBlockPosition[i].x, m_aBlockPosition[i].y);
-	}
-	void Display(){
+	}//Show .. for test.
+};//BlockType_B
 
+// C .. G
+
+class TetrisBoard{
+private:
+	EBUnitColor aBoard[BOARD_SIZE_ROW][BOARD_SIZE_COL];
+protected:
+public:
+	TetrisBoard(){
+		::memset(this, 0, sizeof(TetrisBoard));
+	}
+	~TetrisBoard(){}
+
+	void SetBoard(int in_x, int in_y, EBUnitColor in_color){
+		aBoard[in_y][in_x] = in_color;
+	}
+
+	void ClearBoard(){ ::memset(this, 0, sizeof(TetrisBoard)); }
+
+	void Show(){
+		printf("     < ShowThis >\r\n");
+		printf("     current status of 'aBoard' is ..\r\n");
+		for (int y = 0; y < BOARD_SIZE_ROW; ++y){
+			for (int x = 0; x < BOARD_SIZE_COL; ++x){
+				if (aBoard[y][x] == EMPTY)
+					printf(" .");
+				else
+					printf(" #");
+			}// for-x
+			printf("\r\n");
+		}//for-y
+
+	}//Show .. for test.
+};
+
+class TetrisGame{
+private:
+	BlockQuad* pcCurrentBlock;
+	TetrisBoard cWorld[2];
+	int iCurrentBlockPos_x;
+	int iCurrentBlockPos_y;
+protected:
+public:
+	TetrisGame(){
+		pcCurrentBlock = nullptr;
+	}
+	~TetrisGame(){ DeleteBlock(); }
+
+	// getter
+	const int GetCurrentBlockPos_x(){ return iCurrentBlockPos_x; }
+	const int GetCurrentBlockPos_y(){ return iCurrentBlockPos_y; }
+
+	void Init(){
+		CreateBlock('b');
+		iCurrentBlockPos_x = 9;
+		iCurrentBlockPos_y = 9;
+	}
+	void CreateBlock(char in_type){
+		if (in_type == 'a')
+			pcCurrentBlock = new BlockType_A();
+		else if (in_type == 'b')
+			pcCurrentBlock = new BlockType_B();
+	}
+	
+	void DeleteBlock(){ delete pcCurrentBlock; }
+
+	void UpdateCurrentBlock(){
+		if(pcCurrentBlock)
+			pcCurrentBlock->SetRotation();
+	}
+	void Local_to_Global(int xBoard_, int yBoard_){
+	//void TransLocalBlockToGlobalWorld(int xBoard_, int yBoard_){
+		const BlockUnit* pcaLocalUnit[MAX_BLOCK_SIZE] = { nullptr, nullptr, nullptr, nullptr };
+
+		cWorld[BOARD_BACK].ClearBoard();
+
+		if (pcCurrentBlock){
+			for (int iBlock = 0; iBlock < MAX_BLOCK_SIZE; ++iBlock){
+				pcaLocalUnit[iBlock] = pcCurrentBlock->GetUnit(iBlock);
+				//test //cGlobalQuad.SetUnit(iBlock, pcaLocalUnit[iBlock]->x + xBoard_, pcaLocalUnit[iBlock]->y);
+				const int xGlobal = pcaLocalUnit[iBlock]->x + xBoard_;
+				const int yGlobal = pcaLocalUnit[iBlock]->y + yBoard_;
+
+				if (yGlobal >= 0 && yGlobal < BOARD_SIZE_ROW
+					&& xGlobal >= 0 && xGlobal < BOARD_SIZE_COL){
+					cWorld[BOARD_BACK].SetBoard(xGlobal, yGlobal, DEFAULT);
+				}
+			}
+		}
+		else
+			; // error, there is no block to excute this function.
+	}
+
+
+	void Show(){
+		cWorld[BOARD_BACK].Show();
 	}
 };
-//BlockType_B // .. B - - - o
-//BlockType_C // .. C o - - -
-//...to G (to 7th)
 
 void main()
 {
-	BlockQuad* pTest;
-
-	char TestBlock = 0;
+	TetrisGame test;
 	char input = 0;
 
-	std::cout << "Select Block-Type" << std::endl
-		<< "  Type A : a, A" << std::endl
-		<< "  Type B : b, B" << std::endl
-		<< "  Type C : c, C" << std::endl
-		<< "  Type D : d, D" << std::endl
-		<< "  Type E : e, E" << std::endl
-		<< "  Type F : f, F" << std::endl
-		<< "  Type G : g, G" << std::endl
-		<< "  _quit_ : ESC" << std::endl;
-		//<< "  command> " << std::endl;
+	g_hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	while (1){
-		std::cout << "   command> ";
-		std::cin >> TestBlock;
-		if (TestBlock == 'a' || TestBlock == 'A'){
-			pTest = new BlockType_A(true);//!!new(1)
-			break;
-		}
-		//else if (TestBlock == 'b' || TestBlock == 'B'){
-		//	pTest = new BlockType_B(true);
-		//	break;
-		//}
-		else
-		{
-			std::cout << "   ! warning (wrong Block-Type)" << std::endl;
-			continue;
-		}
-	}
+	test.Show();
+	
+	_getch();
+	system("cls");
+	test.Init();
 
-	//pTest->ShowMe();	// 부모의 함수가 아닌, 재 정의된 자식의 ShowMe()를 불러온다 -- 어떻게?
-						// 그러나, 생성자에서 초기화되었어야 하는 m_ciBlockLimiter을 가져오지는 못한다.
-						// 당연한 이야기이고, 해서도 안될 짓..
+	test.Local_to_Global(test.GetCurrentBlockPos_x(), test.GetCurrentBlockPos_y());
+	SetCursorPosition(0, 0);
+	test.Show();
 
-	pTest->Show();		// ... 인 줄 알았는데, 기본생성자를 호출하는 new의 특성상 ciBlockLimiter를 2로 초기화 시키는 것을 목격..
-						// 부모에게는 없는 멤버를 참조하는 것은 가능해서는 안되는 것 아닌가?
-						// '허가' 되지 않은 영역. 참조, http://www.soen.kr/lecture/ccpp/cpp3/30-1-1.htm
-
-	std::cout << "Testing (SetRotation)" << std::endl
-		<< "   a key : Proceed" << std::endl
-		<< "   s key : Stop and Quit" << std::endl;
+	//_kbhit()
 	while (1)
 	{
 		if (_kbhit())
 		{
+			system("cls");
 			input = _getch();
 
-			if (input == 'a')
-				pTest->SetRotation(true);
+			if (input == 'q')
+				test.UpdateCurrentBlock();
 			else if (input == 's')
 				break;
 			else
-				continue;
+				;
+
+			test.Local_to_Global(test.GetCurrentBlockPos_x(), test.GetCurrentBlockPos_y());
+			SetCursorPosition(0, 0);
+			test.Show();
 		}
 	}
-	//BlockType_A TestOff(false);
-	//return;
-
-	delete(pTest);//!!delete(1)
 }
-*/
-/* 
-	void (BlockQuad::*pFunc2)(void);
-	pFunc2 = (void(BlockType_A::*)(void))(pTest->SetRotation);
-	
-	void (BlockType_A::*pFunc)(void) = &BlockType_A::SetRotation;
-	//pFunc = (void(*)(void))(pTest->SetRotation);
-
-	//void(*pFunc2)(void) = pTest->SetRotation;
-	//void(*pFunc2)(void) = &SetRotation();
-	//cannot convert form 'overloaded-function' to 'void (__cdecl *)(void)'
-
-	... ㅜㅜ ...
-*/
-
-/* 기존 코드에서 쓰던..
-
-
-
-*/
-
-// 좌표변환은 월드에서 하는 것이 맞을 것 같다.
-//	void DrawBlockOnBoard(/*const KBlock* this_,*/ int xBoard_, int yBoard_) {
-//		for (int iBlock = 0; iBlock < this->m_iNumBlock; ++iBlock) {
-//			// transform local coordinate to global coordinate
-//			const int xGlobal = this->m_aBlockPosition[iBlock].x + xBoard_;
-//			const int yGlobal = this->m_aBlockPosition[iBlock].y + yBoard_;
-//			// draw a block on the board
-//			if (yGlobal >= 0 && yGlobal < BOARD_SIZE_ROW
-//				&& xGlobal >= 0 && yGlobal < BOARD_SIZE_COL) {
-//				g_aBoard[yGlobal][xGlobal] = '#';
-//			}//if
-//		}//for
-//	}//DrawBlockOnBoard()
-//};//struct KBlock
